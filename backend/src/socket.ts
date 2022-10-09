a2256df55371115bc0521d3bda36c14f15b23e1c
Filippo Leporati <filippo.leporati@mail.polimi.it>
2022-10-09 14:29:18 +0200

    Add cors configuration backend server

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -4,8 +4,9 @@ import { Server } from "socket.io";
 export default (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: "*",
-    },
+      origin: '*',
+      methods: ['GET', 'POST'],
+  },
   });
 
   io.use((socket, next) => {
