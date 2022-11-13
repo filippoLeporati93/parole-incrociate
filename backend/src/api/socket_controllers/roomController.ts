bbab18bf2999949cdd30a86de492ee896fc92656
Filippo Leporati <filippo.leporati@mail.polimi.it>
2022-11-13 13:09:48 +0100

    backend-custom room mgmt

--- backend/src/api/socket_controllers/roomController.ts
+++ backend/src/api/socket_controllers/roomController.ts
@@ -20,31 +20,50 @@ export class RoomController {
 
     let roomIdJoined: string | null = null;
 
-    const rooms = io.sockets.adapter.rooms;
-    try {
-      for(const [roomId, sockets] of rooms) {
-        if(roomId.startsWith("room_") && sockets.size < 2) {
-          await socket.join(roomId);
-          socket.emit("room_joined", {status: true});
-          roomIdJoined = roomId;
-        }
+    // if the message as already a join roomId use that
+    if ('roomId' in message && message.roomId !== '') {
+      const roomId = message.roomId;
+      const sockets = io.sockets.adapter.rooms.get(roomId) ?? new Set();
+      if (sockets.size < 2) {
+        await socket.join(roomId);
+        socket.emit("room_joined", { status: true });
       }
-    } catch (err) {
-      socket.emit("room_join_error", {
-        error: "Error in joining the room!",
-      });
-    }
-
-    if (!roomIdJoined) {
-      await socket.join("room_" + socket.id)
-      socket.emit("room_joined", {status: true});
-    } else {
-      const sockets = io.sockets.adapter.rooms.get(roomIdJoined) ?? new Set();
+      // if the room you joined is ready and full, emit the start_game event to the players in the room.
       if (sockets.size === 2) {
-        socket.to(roomIdJoined).emit("start_game", { start: false, roomId: roomIdJoined});
-        socket.emit("start_game", { start: true, roomId: roomIdJoined})
+        socket.to(roomId).emit("start_game", { start: false, roomId: roomIdJoined });
+        socket.emit("start_game", { start: true, roomId: roomIdJoined })
+      }
+    } else {
+      // no roomId in the message
+      // search for an available RANDOM room and join it  
+      const rooms = io.sockets.adapter.rooms;
+      try {
+        for (const [roomId, sockets] of rooms) {
+          if (roomId.startsWith("room_") && sockets.size < 2) {
+            await socket.join(roomId);
+            socket.emit("room_joined", { status: true });
+            roomIdJoined = roomId;
+          }
+        }
+      } catch (err) {
+        socket.emit("room_join_error", {
+          error: "Error in joining the room!",
+        });
+      }
+
+      // if no room are available create a new one and join it, then wait for other players
+      if (!roomIdJoined) {
+        await socket.join("room_" + socket.id)
+        socket.emit("room_joined", { status: true });
+      } else {
+        // if the room you joined is ready and full, emit the start_game event to the players in the room.
+        const sockets = io.sockets.adapter.rooms.get(roomIdJoined) ?? new Set();
+        if (sockets.size === 2) {
+          socket.to(roomIdJoined).emit("start_game", { start: false, roomId: roomIdJoined });
+          socket.emit("start_game", { start: true, roomId: roomIdJoined })
+        }
       }
     }
   }
-  
+
 }
